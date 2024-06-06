1ad1e343531099640e40b221bbc6d293990d363a
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-06-06 19:16:21 +0000

    Add kinde log out button

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,9 +4,13 @@ import { Button } from "@/components/ui/button";
 import {
   RegisterLink,
   LoginLink,
+  LogoutLink,
 } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const { isAuthenticated } = getKindeServerSession();
+
   return (
     <nav className="border-b bg-background h-[10vh] flex items-center">
       <div className="container flex items-center justify-between">
@@ -15,14 +19,21 @@ export default function Navbar() {
         </Link>
         <div className="flex items-center gap-x-5">
           <ThemeToggle />
-          <div className="flex items-center gap-x-5">
-            <LoginLink>
-              <Button>Sign In</Button>
-            </LoginLink>
-            <RegisterLink>
-              <Button variant={"secondary"}>Sign Up</Button>
-            </RegisterLink>
-          </div>
+
+          {(await isAuthenticated()) ? (
+            <LogoutLink>
+              <Button>Log out</Button>
+            </LogoutLink>
+          ) : (
+            <div className="flex items-center gap-x-5">
+              <LoginLink>
+                <Button>Sign In</Button>
+              </LoginLink>
+              <RegisterLink>
+                <Button variant={"secondary"}>Sign Up</Button>
+              </RegisterLink>
+            </div>
+          )}
         </div>
       </div>
     </nav>
