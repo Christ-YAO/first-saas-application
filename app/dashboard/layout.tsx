2cd4adf8c2d6bb983aa9489b255b2b2a9b49a5d4
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-06-21 19:32:20 +0000

    Create Layout

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,16 @@
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex flex-col space-y-6 mt-10">
+      <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
+        <aside className="hidden w-[200px] flex-col md:flex">
+          <h1>HELLO</h1>
+        </aside>
+        <main>{children}</main>
+      </div>
+    </div>
+  );
+}
