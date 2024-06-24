a7fd81ac8df9621146b71bc300903f9ff4f5528e
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-06-24 19:29:34 +0000

    Add the toggle theme in nav

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,7 +1,25 @@
-import Logo from '@/public/next.svg'
+import Logo from "/app/favicon.ico";
+import { ThemeToggle } from "@/components/theme/ThemeToggle";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Nav() {
-    return (
-        <div></div>
-    );
+  return (
+    <nav className="max-w-[1200px] w-full mx-auto h-[80px] flex items-center justify-between p-5 border-b border-gray-300">
+      <div className="">
+        <Link href={"/"}>
+          <Image
+            height={30}
+            width={35}
+            src={Logo}
+            alt="Logo Nextjs"
+            className=" dark:bg-white p-[1px] rounded-full"
+          />
+        </Link>
+      </div>
+      <div className="flex items-center gap-4">
+        <ThemeToggle />
+      </div>
+    </nav>
+  );
 }
