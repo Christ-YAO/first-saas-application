0e16e42989ce3aa21ae436c99fd6e353b266508a
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-06-25 20:05:08 +0000

    Implementing NextAuth and prisma

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme/ThemeProvider";
 import Header from "@/components/layout/Header";
+import SessionWrapper from "../lib/SessionWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,8 +21,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <Header />
-          {children}
+          <SessionWrapper>
+            <Header />
+            {children}
+          </SessionWrapper>
         </ThemeProvider>
       </body>
     </html>
