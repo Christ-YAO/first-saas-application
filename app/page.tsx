be218638f61cfeb0a65733d5507460e36a883adc
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-06-26 19:22:47 +0000

    make page redirect when logged in

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,20 @@ import Logo from "/public/vercel.svg";
 import ButtonProvider from "@/components/features/auth/ButtonProvider";
 import TypeWriter from "./_components/TypeWriter";
 import { Card } from "@/components/ui/card";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const session = await getAuthSession();
+
+  if (session) {
+    redirect('/dashboard/notes');
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-2">
-      <div className="w-full h-screen flex items-center justify-center flex-col gap-2 space-y-4">
-        <Card className="p-4 max-w-[600px] hover:bg-accent/5 transition-all container overflow-auto">
+      <div className="w-full h-screen flex items-center justify-center flex-col gap-2">
+        {/* <Card className="p-4 max-w-[600px] hover:bg-accent/5 transition-all container overflow-auto">
           {JSON.stringify(session, null, 2)}
-        </Card>
+        </Card> */}
         <Image
           width={100}
           height={100}
