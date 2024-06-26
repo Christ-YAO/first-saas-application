7d51b6716595c88ba3b0fce5aac9ccbc4afc4661
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-06-26 18:47:17 +0000

    Implementing authentication with google and google

--- app/page.tsx
+++ app/page.tsx
@@ -6,24 +6,28 @@ import TypeWriter from "./_components/TypeWriter";
 import { Card } from "@/components/ui/card";
 
 export default async function Home() {
-  const session = await getAuthSession()
+  const session = await getAuthSession();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-2">
       <div className="w-full h-screen flex items-center justify-center flex-col gap-2 space-y-4">
-      <Card className="p-4 max-w-[600px] hover:bg-accent/10 transition-all">{JSON.stringify(session, null, 2)}</Card>
-      <Image
-        width={100}
-        height={100}
-        alt="Logo Vercel"
-        src={Logo}
-        className="mb-4 object-contain bg-accent p-2 rounded"
-      />
-      <h1 className="text-4xl md:text-6xl font-black mb-2 text-center uppercase flex items-center font-mono">
-        <TypeWriter />
-      </h1>
-      <p className="my-2 text-center font-light dark:font-extralight">Join a coding adventure 💻.</p>
-      <ButtonProvider />
-    </div>
+        <Card className="p-4 max-w-[600px] hover:bg-accent/5 transition-all container overflow-auto">
+          {JSON.stringify(session, null, 2)}
+        </Card>
+        <Image
+          width={100}
+          height={100}
+          alt="Logo Vercel"
+          src={Logo}
+          className="mb-4 object-contain bg-accent p-2 rounded"
+        />
+        <h1 className="text-4xl md:text-6xl font-black mb-2 text-center uppercase flex items-center font-mono">
+          <TypeWriter />
+        </h1>
+        <p className="my-2 text-center font-light dark:font-extralight">
+          Join a coding adventure 💻.
+        </p>
+        <ButtonProvider />
+      </div>
     </main>
   );
 }
--- src/components/features/auth/ButtonProvider.tsx
+++ src/components/features/auth/ButtonProvider.tsx
@@ -8,28 +8,46 @@ import Loader from "@/components/ui/loader";
 import { LogoutButton } from "./LogoutButton";
 
 export default function ButtonProvider() {
-  const mutation = useMutation({
+  const githubMmutation = useMutation({
     // mutationKey: "signIn",
-    mutationFn: async () => signIn(),
+    mutationFn: async () => signIn("github"),
+    // onSuccess: () => window.location.reload(),
+    onError: () => window.location.reload(),
+  });
+
+  const googleMmutation = useMutation({
+    // mutationKey: "signIn",
+    mutationFn: async () => signIn("google"),
     // onSuccess: () => window.location.reload(),
     onError: () => window.location.reload(),
   });
 
   return (
     <div className="flex flex-col space-y-4">
-      <Button variant={"outline"} className="flex gap-4 items-center text-xs">
-        <Chrome size={20} />
+      <Button
+        variant={"outline"}
+        className="flex gap-4 items-center text-xs"
+        disabled={googleMmutation.isPending}
+        onClick={() => {
+          googleMmutation.mutate();
+        }}
+      >
+        {githubMmutation.isPending ? (
+          <Loader className="mr-2" size={12} />
+        ) : (
+          <Chrome className="mr-2" size={20} />
+        )}
         Continue with Google
       </Button>
       <Button
         variant={"outline"}
         className="flex gap-4 items-center text-xs"
-        disabled={mutation.isPending}
+        disabled={githubMmutation.isPending}
         onClick={() => {
-          mutation.mutate();
+          githubMmutation.mutate();
         }}
       >
-        {mutation.isPending ? (
+        {githubMmutation.isPending ? (
           <Loader className="mr-2" size={12} />
         ) : (
           <Github className="mr-2" size={20} />
