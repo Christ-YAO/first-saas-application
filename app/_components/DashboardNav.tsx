043a4c3358ff9dde91496547ec37d4bde4b71eba
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-06-27 09:27:18 +0000

    nav links styles

--- app/_components/DashboardNav.tsx
+++ app/_components/DashboardNav.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import { CreditCard, Notebook, Settings } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -18,13 +19,20 @@ export default function DashboardNav() {
       {menuDashboard.map((link, index) => {
         const isActive = pathname?.startsWith(link.path);
         return (
-          <Link href={link.path} key={index} className={buttonVariants({variant: "outline"})}>
-            <div
-              className={`flex items-center justify-center lg:justify-start gap-2 cursor-pointer lg:p-3 p-2 hover:bg-orange-500 hover:bg-opacity-50 hover:text-white text-sm font-bold rounded-md transition-all ${
+          <Link
+            href={link.path}
+            key={index}
+            className={cn(
+              buttonVariants({ variant: "outline" }),
+              `flex items-center justify-center lg:justify-start gap-2 lg:p-3 p-2 hover:bg-orange-500 hover:bg-opacity-80 hover:text-white text-sm font-light rounded-md transition-all ${
                 isActive && "bg-orange-500 text-white"
-              }`}
+              }`
+            )}
+          >
+            <div
+              className={`flex items-center justify-center lg:justify-start gap-2`}
             >
-                <link.icon className="w-4" />
+              <link.icon className="w-4" />
               <span className="hidden lg:block">{link.name}</span>
             </div>
           </Link>
