2c1c0265f7b983d8483ab9517d6e5265f6f9f151
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-06-27 15:03:31 +0000

    Add react-toastify

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,8 @@
 import { getAuthSession } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import DashboardNav from "../_components/DashboardNav";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default async function DashboardLayout({
   children,
@@ -11,9 +13,12 @@ export default async function DashboardLayout({
     redirect("/");
   }
   return (
-    <section className="max-w-[1200px] mx-auto md:flex md:items-center md:gap-4 h-screen w-full mt-2 p-2">
+    <section className="max-w-[1200px] mx-auto md:flex md:items-start md:gap-6 gap-4 h-screen w-full mt-2 p-2">
       <DashboardNav />
-      <div className="w-full h-full">{children}</div>
+      <div className="w-full h-full">
+        {children}
+        <ToastContainer />
+      </div>
     </section>
   );
 }
