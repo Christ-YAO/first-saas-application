8ab7a4b32f77e4b9af23565531cb38095d5318c4
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-06-28 19:16:53 +0000

    Notes page when have no note

--- app/dashboard/notes/page.tsx
+++ app/dashboard/notes/page.tsx
@@ -3,7 +3,7 @@ import { Card } from "@/components/ui/card";
 import { getAllNotes } from "@/lib/actionsNotes";
 import { getUser } from "@/lib/actionsUsers";
 import { cn } from "@/lib/utils";
-import { FilePenLine } from "lucide-react";
+import { FileArchive, FilePenLine, PlusCircle } from "lucide-react";
 import Link from "next/link";
 import ButtonDelete from "../../_components/ButtonDelete";
 
@@ -30,7 +30,25 @@ export default async function NotesPage() {
       </div>
 
       {notes.length < 1 ? (
-        <div className="flex min-h-[400px] flex-col items-center justify-center rounded-md border border-dashed p-3"></div>
+        <div className="flex min-h-[400px] flex-col items-center justify-center rounded-md border border-dashed p-3">
+          <div className="w-16 h-16 rounded-full flex items-center justify-center bg-orange-600 bg-opacity-20 mb-4">
+            <FileArchive className="text-orange-600" />
+          </div>
+          <p className="text-white">You have no Note</p>
+          <p className="text-muted-foreground text-sm">
+            Start creating notes now through our app
+          </p>
+          <Link
+            href={"/dashboard/notes/create"}
+            className={cn(
+              buttonVariants({ variant: "secondary" }),
+              `font-light mt-4 flex items-center gap-2`
+            )}
+          >
+            Create a new note
+            <PlusCircle size={12} className="mt-1" />
+          </Link>
+        </div>
       ) : (
         <div className="flex flex-col space-y-4">
           {notes.map((item, index) => (
@@ -50,7 +68,7 @@ export default async function NotesPage() {
                 <Link
                   href={`notes/note/${item.id}`}
                   className={cn(
-                    buttonVariants({variant: "ghost"}),
+                    buttonVariants({ variant: "ghost" }),
                     "bg-orange-600 hover:bg-opacity-80 hover:bg-orange-600 transition-all text-white"
                   )}
                 >
