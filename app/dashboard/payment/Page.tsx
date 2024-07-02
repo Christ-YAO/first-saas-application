f8883491c2c3a3a76e56a04e59f7203da8f9979e
Christ-YAO <114085019+Christ-YAO@users.noreply.github.com>
2024-07-02 20:04:19 +0000

    payment page when have subscription

--- app/dashboard/payment/Page.tsx
+++ app/dashboard/payment/Page.tsx
@@ -1,14 +1,60 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
+import {
+  createCustomerPortal,
+  createSubscription,
+  getDataStripeUser,
+} from "@/lib/actionsStripe";
+import { getUser } from "@/lib/actionsUsers";
 import { CheckCircle, CreditCard, Euro } from "lucide-react";
+import Image from "next/image";
 
-export default function PaymentPage() {
+export default async function PaymentPage() {
+  const user = await getUser();
+
+  const dataStripe = await getDataStripeUser(user?.id as string);
+
   const itemsPremium = [
     { name: "Hébergement web fiable et sécurisé" },
     { name: "Conception Responsive et Conviviale" },
     { name: "Fonctionnalités Avancées" },
     { name: "Support Technique et Mises à jour" },
   ];
+
+  if (dataStripe?.status === "active") {
+    return (
+      <div className="maw-w-lg mx-auto space-y-4 my-3">
+        <Card className="flex flex-col">
+          <CardContent className="py-8">
+            <div>
+              <h3 className="text-md font-black uppercase bg-orange-900 bg-opacity-20 text-orange-600 p-3 rounded-md inline font-mono">
+                premium pass
+              </h3>
+              <p className="mt-4 text-sm text-muted-foreground">
+                Modifier votre abonnement premium
+              </p>
+              <Image
+                src={"/badge.png"}
+                width={100}
+                height={100}
+                alt="badge"
+                className="block my-4"
+              />
+              <form action={createCustomerPortal} className="w-full">
+                <Button
+                  className="bg-orange-600 hover:bg-orange-700 hover:bg-opacity-80 font-mono font-bold transition-all text-white flex items-center gap-1 w-full"
+                  type="submit"
+                >
+                  <CreditCard size={16} /> Sauvegarder les modifications
+                </Button>
+              </form>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
+
   return (
     <div className="max-w-lg mx-auto space-y-4 mt-3">
       <Card className="flex flex-col">
@@ -38,7 +84,7 @@ export default function PaymentPage() {
                 </li>
               ))}
             </ul>
-            <form action="" className="w-full mt-4">
+            <form action={createSubscription} className="w-full mt-4">
               <Button className="bg-orange-600 hover:bg-orange-700 hover:bg-opacity-80 font-mono font-bold transition-all text-white flex items-center gap-1">
                 <Euro size={16} /> Become a premium member
               </Button>
